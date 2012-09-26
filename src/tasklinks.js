749ed7a7e3257b2a376bf8dc6a32b3e699852e7b
B3nGr33ni3r <mygithub@b3nhub.com>
2012-09-25 23:04:11 -0400

    eww

--- src/tasklinks.js
+++ src/tasklinks.js
@@ -5,7 +5,7 @@ function tasklink()
 	{
 		console.log("Tasklink loaded method called.");
 		
-		this.storage.Tasklist.List = this.query.Tasklist.List();
+		this.query.Tasklist.List();
 		var givenId="";//parse this from the above
 		this.storage.Tasklist.Get = this.query.Tasklist.Get(givenId);
 		console.log(this.storage);
@@ -26,104 +26,104 @@ function tasklink()
 			{https://developers.google.com/google-apps/tasks/v1/reference/tasklists/list
 				return gapi.client.HttpRequest.execute(gapi.client.request({
 					"path" : "https://www.googleapis.com/tasks/v1/users/@me/lists"
-				}));
+				}).execute(function(a,b){console.log(a);});
 			},
 			Get : function(a)
 			{//https://developers.google.com/google-apps/tasks/v1/reference/tasklists/get
-				return gapi.client.HttpRequest.execute(gapi.client.request({
+				return gapi.client.request({
 					"path" : "https://www.googleapis.com/tasks/v1/users/@me/lists/"+a
-				}));
+				});
 			},
 			Insert : function(a)
 			{//https://developers.google.com/google-apps/tasks/v1/reference/tasklists/insert
-				return gapi.client.HttpRequest.execute(gapi.client.request({
+				return gapi.client.request({
 					"path" : "https://www.googleapis.com/tasks/v1/users/@me/lists/",
 					"method" : "POST",
 					"body" : a
-				}));
+				});
 			},
 			Update : function(a,b)
 			{//https://developers.google.com/google-apps/tasks/v1/reference/tasklists/update
-				return gapi.client.HttpRequest.execute(gapi.client.request({
+				return gapi.client.request({
 					"path" : "https://www.googleapis.com/tasks/v1/users/@me/lists/"+a,
 					"method" : "PUT",
 					"body" : b
-				}));
+				});
 			},
 			Delete : function(a)
 			{//https://developers.google.com/google-apps/tasks/v1/reference/tasklists/delete
-				return gapi.client.HttpRequest.execute(gapi.client.request({
+				return gapi.client.request({
 					"path" : "https://www.googleapis.com/tasks/v1/users/@me/lists/"+a,
 					"method" : "DELETE"
-				}));
+				});
 			},
 			Patch : function(a,b)
 			{//https://developers.google.com/google-apps/tasks/v1/reference/tasklists/patch
-				return gapi.client.HttpRequest.execute(gapi.client.request({
+				return gapi.client.request({
 					"path" : "https://www.googleapis.com/tasks/v1/users/@me/lists/"+a,
 					"method" : "PATCH",
 					"body" : b
-				}));
+				});
 			}
 			
 		},
 		Tasks : {
 			List : function(a)
 			{//https://developers.google.com/google-apps/tasks/v1/reference/tasks/list
-				return gapi.client.HttpRequest.execute(gapi.client.request({
+				return gapi.client.request({
 					"path" : "https://www.googleapis.com/tasks/v1/users/@me/lists/"+a+"/tasks"
-				}));
+				});
 			},
 			Get : function(a,b)
 			{//https://developers.google.com/google-apps/tasks/v1/reference/tasks/get
-				return gapi.client.HttpRequest.execute(gapi.client.request({
+				return gapi.client.request({
 					"path" : "https://www.googleapis.com/tasks/v1/lists/"+a+"/tasks/"+b
-				}));
+				});
 			},
 			Insert : function(a,b)
 			{//https://developers.google.com/google-apps/tasks/v1/reference/tasks/insert
-				return gapi.client.HttpRequest.execute(gapi.client.request({
+				return gapi.client.request({
 					"path" : "https://www.googleapis.com/tasks/v1/lists/"+a+"/tasks/",
 					"method" : "POST",
 					"body" : b
-				}));
+				});
 			},
 			Update : function(a,b,c)
 			{//https://developers.google.com/google-apps/tasks/v1/reference/tasks/update
-				return gapi.client.HttpRequest.execute(gapi.client.request({
+				return gapi.client.request({
 					"path" : "https://www.googleapis.com/tasks/v1/lists/"+a+"/tasks/"+b,
 					"method" : "PUT",
 					"body" : c
-				}));
+				});
 			},
 			Delete : function(a,b)
 			{//https://developers.google.com/google-apps/tasks/v1/reference/tasks/delete
-				return gapi.client.HttpRequest.execute(gapi.client.request({
+				return gapi.client.request({
 					"path" : "https://www.googleapis.com/tasks/v1/lists/"+a+"/tasks/"+b,
 					"method" : "DELETE"
-				}));
+				});
 			},
 			Clear : function(a)
 			{//https://developers.google.com/google-apps/tasks/v1/reference/tasks/clear
-				return gapi.client.HttpRequest.execute(gapi.client.request({
+				return gapi.client.request({
 					"path" : "https://www.googleapis.com/tasks/v1/lists/"+a+"/clear",
 					"method" : "POST"
-				}));
+				});
 			},
 			Move : function(a,b,parent)
 			{//https://developers.google.com/google-apps/tasks/v1/reference/tasks/move
-				return gapi.client.HttpRequest.execute(gapi.client.request({
+				return gapi.client.request({
 					"path" : "https://www.googleapis.com/tasks/v1/lists/"+a+"/tasks/"+b+"/move",
 					"method" : "POST"
-				}));
+				});
 			},
 			Patch : function(a,b,c)
 			{//https://developers.google.com/google-apps/tasks/v1/reference/tasks/patch
-				return gapi.client.HttpRequest.execute(gapi.client.request({
+				return gapi.client.request({
 					"path" : "https://www.googleapis.com/tasks/v1/lists/"+a+"/tasks/"+b,
 					"method" : "PATCH",
 					"body" : c
-				}));
+				});
 			},
 		}
 		
